c124df9e283c9dad9060aa7d0a37e9e1090f1e13
ellos0 <mcgibbonellott@gmail.com>
2025-11-05 15:42:52 -0800

    moved window class to seperate file

---
+++ src/window.py
@@ -0,0 +1,13 @@
+class Window:
+  def __init__(self,height,width,y,x):
+    self.height = height
+    self.width = width
+    self.y = y
+    self.x = x
+    self.win = curses.newwin(height,width,y,x)
+  def box(self):
+    self.win.box()
+  def refresh(self):
+    self.win.refresh()
+  def write(self,y,x,text):
+    self.win.addstr(y,x,text)
