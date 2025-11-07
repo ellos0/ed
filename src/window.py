88a815dab67ec96c19a49b6f19935bab0967b5a3
ellos0 <mcgibbonellott@gmail.com>
2025-11-06 21:39:40 -0800

    more window stuff

--- src/window.py
+++ src/window.py
@@ -1,15 +1,28 @@
 import curses
+import line
+
+#window logic and functions
 
 class Window:
-  def __init__(self,height,width,y,x):
+  def __init__(self,height,width,y=1,x=1,buffer=[""], cursor=[0,0]):
     self.height = height
     self.width = width
     self.y = y
     self.x = x
+    self.edit = line.Edit(buffer,cursor)
     self.win = curses.newwin(height,width,y,x)
-  def box(self):
-    self.win.box()
-  def refresh(self):
-    self.win.refresh()
-  def write(self,y,x,text):
-    self.win.addstr(y,x,text)
+
+def box(w):
+  w.win.box()
+
+def refresh(w):
+  w.win.refresh()
+
+def write_cursor(w,text):
+  w.win.addstr(w.edit.cursor[1], w.edit.cursor[0], text)
+
+def write(w,y,x,text):
+  w.win.addstr(y,x,text)
+
+def cursor(w):
+  w.win.move(w.edit.cursor[0],w.edit.cursor[1])
